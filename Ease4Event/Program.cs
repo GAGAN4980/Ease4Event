34a5145ccb53239b9690c44a32c54795b824e9fb
gagandeepsingh <gagandeep4980@gmail.com>
2025-03-23 22:40:42 -0400

    added views

--- Ease4Event/Program.cs
+++ Ease4Event/Program.cs
@@ -36,7 +36,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Event}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
